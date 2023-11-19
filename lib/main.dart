432929a83a841e325a6ea40dd028885e19d8a0ca
DeepeshKalura <deepeshkalurs@gmail.com>
2023-11-19 11:07:01 +0530

    Add Firebase plugins and configuration files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'books_splash.dart';
+// import 'controller/firebase/api_to_firebase.dart';
+import 'controller/firebase/api_to_firebase.dart';
+import 'firebase_options.dart';
 import 'controller/app/home_controller.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  ApiToFirebase apiToFirebase = ApiToFirebase();
+  await apiToFirebase.discoverBooks();
+  await apiToFirebase.addBookToFirebase();
   runApp(const MyApp());
 }
 
@@ -27,7 +38,25 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
           fontFamily: "SourceSansPro",
         ),
-        home: const BooksSplash(),
+        home: const
+            // HomeScreen(),
+            BooksSplash(),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Vani"),
+      ),
+      body: const Center(
+        child: Text("Home Screen"),
       ),
     );
   }
